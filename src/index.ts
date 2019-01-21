e98ff5341619d53b2ed204ef7c01972a064c4cf3
Bruce C. Schardt <schardt.bruce.curtis@gmail.com>
2019-01-21 15:40:40 -0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+
+
+import { ParquetReader ,ParquetSchema, ParquetWriter } from 'parquets'
+
+import { write } from 'fs';
+
+// declare a schema for the `fruits` table
+const schema = new ParquetSchema({
+    name: { type: 'UTF8' },
+    quantity: { type: 'INT64' },
+    price: { type: 'DOUBLE' },
+    date: { type: 'TIMESTAMP_MILLIS' },
+    in_stock: { type: 'BOOLEAN' }
+  });
+
+
+async function  writesome() {
+  // create new ParquetWriter that writes to 'fruits.parquet`
+const writer =  await ParquetWriter.openFile(schema, 'fruits.parquet');
+
+// append a few rows to the file
+await writer.appendRow({name: 'apples', quantity: 10, price: 2.5, date: new Date(), in_stock: true});
+await writer.appendRow({name: 'oranges', quantity: 10, price: 2.5, date: new Date(), in_stock: true});
+await writer.close()
+
+}
+
+async function      readsome() {
+    // create new ParquetWriter that writes to 'fruits.parquet`
+  const reader =  await ParquetReader.openFile('fruits.parquet');
+  
+  // append a few rows to the file
+  const cursor = reader.getCursor(["name","price"]);
+  let record = null;
+  while (record = await cursor.next()){
+      console.log(record)
+  }
+  reader.close();
+}
+writesome();  
+readsome();
